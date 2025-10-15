6a469c8254e27df36223c8c3c118ccd25b433744
Abel Salvatierra <assalvatierra@gmail.com>
2025-10-15 14:38:06 -0700

    Update tests to include HttpClientTestingModule and components
    Added HttpClientTestingModule to the test setups for AppComponent and SampleapiComponent. Also updated AppComponent tests to declare and check for WelcomeComponentComponent and SampleapiComponent, ensuring all relevant components are included and rendered in the test environment.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,15 +1,21 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterModule } from '@angular/router';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { AppComponent } from './app.component';
+import { WelcomeComponentComponent } from './samples/welcome-component/welcome-component.component';
+import { SampleapiComponent } from './samples/sample-apicall/sampleapi/sampleapi.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterModule.forRoot([])
+        RouterModule.forRoot([]),
+        HttpClientTestingModule
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        WelcomeComponentComponent,
+        SampleapiComponent
       ],
     }).compileComponents();
   });
@@ -26,10 +32,11 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('Client');
   });
 
-  it('should render title', () => {
+  it('should render components', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Client');
+    expect(compiled.querySelector('app-welcome-component')).toBeTruthy();
+    expect(compiled.querySelector('app-sampleapi')).toBeTruthy();
   });
 });
--- src/app/samples/sample-apicall/sampleapi/sampleapi.component.spec.ts
+++ src/app/samples/sample-apicall/sampleapi/sampleapi.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { SampleapiComponent } from './sampleapi.component';
 
@@ -8,6 +9,7 @@ describe('SampleapiComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
       declarations: [SampleapiComponent]
     })
     .compileComponents();
