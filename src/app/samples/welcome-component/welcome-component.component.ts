e0fe7315b37f3487e34e853ec5ab29335bdb9f1d
Abel Salvatierra <assalvatierra@gmail.com>
2025-10-15 13:34:46 -0700

    Add WelcomeComponent sample to app module
    Introduced WelcomeComponentComponent with its HTML, CSS, TypeScript, and test files. Registered the new component in app.module.ts to demonstrate a basic welcome message.

---
+++ src/app/samples/welcome-component/welcome-component.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-welcome-component',
+  standalone: false,
+  templateUrl: './welcome-component.component.html',
+  styleUrl: './welcome-component.component.css'
+})
+export class WelcomeComponentComponent {
+  title: string = 'Welcome to Our Application';
+}
